719a0ccab6f26a5ac71c2d13856695c9c31b5970
Alex Johnson <al.johnson@hotmail.com>
2024-10-17 20:06:59 -0400

    removing layout

--- src/Budgeting-app/client/src/App.js
+++ src/Budgeting-app/client/src/App.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
 import Home from "./Home";
 import FinancialData from "./FinancialData";
 import Summary from "./Summary";
