697c886fe88f9c15afc55ec2f2aa5e268374924c
Matt Keuning <keuninma@mail.gvsu.edu>
2024-10-21 17:09:04 -0400

    Attempting to add spinner to submit button and changed js to be conditional based depending on which page the user is on

--- website/app.js
+++ website/app.js
@@ -23,18 +23,40 @@ var usernamefield = document.querySelector("#usernamefield1");
 var passwordfield = document.querySelector("#passwordfield1");
 var submit = document.querySelector("#submit1");
 
-function submitData() {
-    set(ref(db, "Users/" + usernamefield.value), {
-        User: usernamefield.value,
-        Pass: passwordfield.value
-    })
-    .then(()=>{
-        alert("Data added successfully.")
-    })
-    .catch((error)=>{
-        //alert(error)
-    })
 
+if (submit) {
+    function submitData() {
+        set(ref(db, "Users/" + usernamefield.value), {
+            User: usernamefield.value,
+            Pass: passwordfield.value
+        })
+        .then(()=>{
+            alert("Data added successfully.")
+        })
+        .catch((error)=>{
+            //alert(error)
+        })
+    
+    }
+
+    submit.addEventListener('click', submitData);
 }
 
-submit.addEventListener('click', submitData);
+
+
+const submitform = document.getElementById('submitform');
+
+if (submitform) {
+    function loadspin(e) {
+        e.preventDefault(); // Prevent the default form submission
+        submitform.classList.add('loading');
+        // Simulate a delay for form submission
+        setTimeout(() => {
+            submitform.classList.remove('loading');
+            alert('Form Submitted'); // Simulate form submission response
+        }, 3000); // 3 seconds delay
+    }
+    
+    submitform.addEventListener('click', loadspin);
+}
+
