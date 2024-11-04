3445e0bf191fba325787532ea5ed4205d4c6dbb6
MattKeuning <mattkeuning1@gmail.com>
2024-11-03 22:02:52 -0500

    Finishing FinancialData.js react components.

--- src/Budgeting-app/client/src/FinancialData/FinancialData.js
+++ src/Budgeting-app/client/src/FinancialData/FinancialData.js
@@ -104,18 +104,197 @@ function FinancialData() {
           <h3 className='prefname'>Preferred Name</h3>
         </div>
         <div>
-        <input
-          className='firstnamebox'
-          type='text'
-          placeholder='First Name'
-          id='firstname1'
-          value={firstName}
-          onChange={(value) => setFirstName(v.target.value)}
-        />
+          <input
+            className='firstnamebox'
+            type='text'
+            placeholder='First Name'
+            id='firstname1'
+            value={firstName}
+            onChange={(value) => setFirstName(v.target.value)}
+          />
         </div>
-
+        <div>
+          <input
+            className='lastnamebox'
+            type='text'
+            placeholder='Last Name'
+            id='lastname1'
+            value={lastName}
+            onChange={(value) => setLastName(v.target.value)}
+          />
+        </div>
+        <h3 className='findata'>Financial Data</h3>
+        <div>
+          <input
+            className='monthin'
+            type='number'
+            placeholder='Total Monthly Income'
+            id='totin'
+            value={monthlyIncome}
+            onChange={(value) => setMonthlyIncome(v.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='monthex'
+            type='number'
+            placeholder='Total Monthly Expenses'
+            id='monthlyExpenses'
+            value={monthlyExpenses}
+            onChange={(value) => setMonthlyExpenses(v.target.value)}
+          />
+        </div>
+        <h3 className='breakdown'>Expense Breakdown</h3>
+        <div>
+          <input
+            className='housing'
+            type='number'
+            placeholder='Housing'
+            id='housing1'
+            value={housing}
+            onChange={(value) => setMonthlyExpenses(v.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='utilities'
+            type='number'
+            placeholder='Utilities'
+            id='utilities1'
+            value={utilities}
+            onChange={(value) => setUtilities(v.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='transportation'
+            type='number'
+            placeholder='Transportation'
+            id='transportation1'
+            value={transportation}
+            onChange={(value) => setTransportation(v.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='food'
+            type='number'
+            placeholder='Food'
+            value={utilities}
+            onChange={(value) => setFood(v.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='debtrepayment'
+            type='number'
+            placeholder='Debtrepayment'
+            id='debtrepayment1'
+            value={debtRepayment}
+            onChange={(value) => setDebtRepayment(v.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='insurnace'
+            type='number'
+            placeholder='Insurance'
+            id='insurance1'
+            value={insurance}
+            onChange={(value) => setInsurance(v.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='health'
+            type='number'
+            placeholder='Health'
+            id='health1'
+            value={health}
+            onChange={(value) => setHealth(v.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='entertainment'
+            type='number'
+            placeholder='Entertainment'
+            id='entertainment1'
+            value={entertainment}
+            onChange={(value) => setEntertainment(v.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='education'
+            type='number'
+            placeholder='Education'
+            id='education1'
+            value={education}
+            onChange={(value) => setEducation(v.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='investments'
+            type='number'
+            placeholder='Investments'
+            id='investments1'
+            value={investments}
+            onChange={(value) => setInvestments(v.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='family'
+            type='number'
+            placeholder='Family Expenses'
+            id='familyexpenses1'
+            value={familyExpenses}
+            onChange={(value) => setFamilyExpenses(v.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='other'
+            type='number'
+            placeholder='Other'
+            id='other1'
+            value={other}
+            onChange={(value) => setOther(v.target.value)}
+          />
+        </div>
+        <h3 className='pinfo'>Personal Related Information</h3>
+        <div>
+          <input
+            className='curage'
+            type='number'
+            placeholder='Current Age'
+            id='cage1'
+            value={currentAge}
+            onChange={(value) => setCurrentAge(v.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='retage'
+            type='number'
+            placeholder='Desired Retirement Age'
+            id='rage1'
+            value={retirementAge}
+            onChange={(value) => setRetirementAge(v.target.value)}
+          />
+        </div>
+        <button
+          type='button'
+          className='submitform'
+          id='submitform1'
+          onClick={onSubmit}
+        >
+          <span className='subtext' id='subtext'><strong>Submit</strong></span>
+        </button>
       </div>
-    )
+    );
 
   }
 
