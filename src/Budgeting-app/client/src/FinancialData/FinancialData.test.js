bd122026bee3a41e9efa544e075db074ea0ea09a
MattKeuning <mattkeuning1@gmail.com>
2024-11-30 15:41:24 -0500

    render unit test for finanical form

--- src/Budgeting-app/client/src/FinancialData/FinancialData.test.js
+++ src/Budgeting-app/client/src/FinancialData/FinancialData.test.js
@@ -1,9 +1,14 @@
+import { MemoryRouter } from "react-router-dom";
 import FinancialData from "./FinancialData";
 
 describe(FinancialData, () => {
 
     it("Financial Data page displays the financial form", () => {
-        pass;
+        renderMatches(
+            <MemoryRouter>
+                <FinancialData />
+            </MemoryRouter>
+        );
     });
 
 });
